98565318afab618f4380adec33798a02f7378c13
ShengUei <shengueiwengtw@gmail.com>
2022-09-03 22:00:23 +0800

    add equal

--- util/float_util.py
+++ util/float_util.py
@@ -1,7 +1,4 @@
-
-
 import math
-
 
 def greatter_than(num1, num2):
     int_1 = math.floor(num1)
@@ -14,3 +11,12 @@ def greatter_than(num1, num2):
         return fraction_1 > fraction_2
 
     return int_1 > int_2
+
+def equal(num1, num2):
+    int_1 = math.floor(num1)
+    fraction_1 = math.floor((num1 - int_1) * 10)
+
+    int_2 = math.floor(num2)
+    fraction_2 = math.floor((num2 - int_2) * 10)
+
+    return int_1 == int_2 and fraction_1 == fraction_2
