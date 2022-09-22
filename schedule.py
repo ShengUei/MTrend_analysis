69043b3e510d072b08f6e9302ce9ab59dd704c6c
ShengUei <shengueiwengtw@gmail.com>
2022-09-22 23:37:51 +0800

    close show_pid

--- schedule.py
+++ schedule.py
@@ -3,7 +3,6 @@ from apscheduler.schedulers.background import BlockingScheduler
 from datetime import datetime, timezone, timedelta
 
 from schedule.exchange_rate_analysis_job import three_day_alert
-from schedule.show_pid_job import show_pid
 from sendEmail.send_email import send_email
 
 three_day_alert()
@@ -18,8 +17,6 @@ try:
 
     #每週一 ~ 五 09:00 ，由 DB 抓匯率並分析
     scheduler.add_job(three_day_alert, 'cron', day_of_week = '1-5', hour = 9, minute = 0, timezone = 'Asia/Taipei')
-
-    scheduler.add_job(show_pid, 'interval', hours = 1)
 
     scheduler.start()
 
